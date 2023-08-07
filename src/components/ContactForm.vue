d703214a0da06f64ae52b78799e8e5000d6161f7
WatCodeDatCode <arussell742@gmail.com>
2023-08-07 15:13:51 +0200

    update logic, add email validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, watch } from "vue";
+import { reactive, computed, watch } from "vue";
 
 interface FormData {
   name: string;
@@ -15,8 +15,13 @@ interface Errors {
 const formData: FormData = reactive({ name: "", email: "", message: "" });
 const errors: Errors = reactive({ email: false, message: false });
 
+const emailIsValid = computed(() => {
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(formData.email);
+});
+
 const validInputs = () => {
-  errors.email = !formData.email;
+  errors.email = !formData.email || !emailIsValid.value;
   errors.message = !formData.message;
   return errors.email || errors.message ? false : true;
 };
@@ -28,15 +33,19 @@ const sendMessage = () => {
   alert(JSON.stringify(formData));
 };
 
-watch(formData, (value) => {
-  if (value.email) errors.email = false;
-  if (value.message) errors.message = false;
-});
+watch(
+  () => ({ ...formData }),
+  (newVal, oldVal) => {
+    console.log({ newVal, oldVal });
+    if (newVal.email !== oldVal.email) errors.email = false;
+    if (newVal.message !== oldVal.message) errors.message = false;
+  }
+);
 </script>
 
 <template>
-  <form @submit.prevent="sendMessage">
-    <div class="form-input">
+  <form novalidate @submit.prevent="sendMessage">
+    <div class="form-input-container">
       <input
         v-model="formData.name"
         class="form-input"
@@ -45,27 +54,31 @@ watch(formData, (value) => {
         placeholder="Your name"
       />
     </div>
-    <div class="form-input">
+    <div class="form-input-container">
       <input
         v-model="formData.email"
         class="form-input"
+        :class="{ error: errors.email }"
         name="email"
         type="email"
         placeholder="Your email address"
       />
-      <p v-if="errors.email" class="form-error-message">
-        Please enter a valid email
-      </p>
+      <span v-if="errors.email" class="form-error-message">
+        Please enter a
+        {{ formData.email && !emailIsValid ? "valid " : "" }}contact email
+      </span>
     </div>
-    <div class="form-input">
+    <div class="form-input-container">
       <textarea
         v-model="formData.message"
-        name="message"
+        class="form-input"
+        :class="{ error: errors.message }"
         placeholder="Your message"
+        name="message"
       />
-      <p v-if="errors.message" class="form-error-message">
+      <span v-if="errors.message" class="form-error-message">
         Please enter a message
-      </p>
+      </span>
     </div>
     <div class="form-button-container">
       <button class="submit-button" type="submit">Submit</button>
@@ -76,7 +89,22 @@ watch(formData, (value) => {
 <style lang="sass" scoped>
 form
   width: 100%
+  min-width: 80vw
+  display: flex
+  flex-direction: column
+
+  > div
+    margin-top: 1rem
 
 input, textarea
   width: 100%
+
+.form-input-container
+  .form-input
+    &.error
+      border: 1px solid red
+
+  .form-error-message
+    font-size: 0.8rem
+    color: red
 </style>
