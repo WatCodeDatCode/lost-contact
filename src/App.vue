7f24591b57d1456288f50ca659ba51fe13ff87ef
WatCodeDatCode <arussell742@gmail.com>
2023-08-07 12:43:25 +0200

    add MainHeader, move logos, add alias

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import ContactForm from "./components/ContactForm.vue";
-import MainFooter from "./components/MainFooter.vue";
+import ContactForm from "@/components/ContactForm.vue";
+import MainHeader from "@/components/MainHeader.vue";
+import MainFooter from "@/components/MainFooter.vue";
 </script>
 
 <template>
+  <MainHeader />
   <div class="page-content">
     <ContactForm />
   </div>
