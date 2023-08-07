dfc6ce31a4bfcccff811d31f8d2cd827ed5ea867
WatCodeDatCode <arussell742@gmail.com>
2023-08-07 11:28:12 +0200

    update favicon, add footer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <script setup lang="ts">
 import ContactForm from "./components/ContactForm.vue";
+import MainFooter from "./components/MainFooter.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="page-content">
+    <ContactForm />
   </div>
-  <ContactForm />
+  <MainFooter />
 </template>
 
-<style lang="sass" scoped>
-.logo
-  height: 6em
-  padding: 1.5em
-  will-change: filter
-  transition: filter 300ms
-
-.logo:hover
-  filter: drop-shadow(0 0 2em #646cffaa)
-
-.logo.vue:hover
-  filter: drop-shadow(0 0 2em #42b883aa)
-</style>
+<style lang="sass" scoped></style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <p>
+      Icon by
+      <a href="https://freeicons.io/profile/5989" target="_blank"
+        >Qaisir Mehmood</a
+      >
+      on <a href="https://freeicons.io" target="_blank">freeicons.io</a>.
+    </p>
+  </footer>
+</template>
+
+<style lang="sass" scoped>
+p
+    font-size: 12px
+</style>
