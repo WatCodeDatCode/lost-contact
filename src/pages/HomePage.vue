f030442b933d7cd4a95b1b06993ba4655340581f
WatCodeDatCode <arussell742@gmail.com>
2023-08-08 09:41:18 +0200

    move content to pages folder

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ContactForm from "@/components/ContactForm.vue";
+</script>
+
+<template>
+  <div class="page-content">
+    <p>
+      Thank you for taking the time to report my missing item(s)! Please fill
+      out the form below and I will contact you as soon as possible!
+    </p>
+    <ContactForm />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+p
+  margin: 0
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ContactForm from "@/components/ContactForm.vue";
+import HomePage from "@/pages/HomePage.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
-      component: ContactForm
+      component: HomePage
     }
   ]
 });
